86f5bc817e59d88cb30ef87f6c5a79e262795bc6
root <root@ip-172-26-5-245.ec2.internal>
2020-04-17 08:31:38 +0000

    Another Draft

---
+++ html/app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Database Parameters
+
+define('DB_HOST','localhost');
+define('DB_USER','admin');
+define('DB_PASS','password');
+define('DB_NAME','covid');
+
+//approot
+define('APPROOT',dirname(dirname(__FILE__)));
+
+//URLROOT
+define('URLROOT','https://amdcr.co.in/');
+define('DS', DIRECTORY_SEPARATOR);
+
+//sitename
+
+define('SITENAME','Covid19-Uttarakhand');
+?>
